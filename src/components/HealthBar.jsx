716e4ec35e4f0a97d373a36109a2fa722ad4ae82
Steven Coy <stevencoy@gmail.com>
2024-07-23 18:23:35 -0700

    Continued improving final battle health bars.

--- src/components/HealthBar.jsx
+++ src/components/HealthBar.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+
 export default function HealthBar({ health }) {
   // Set width of health-bar by health
 
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--health-bar-width",
+      `${health}%`
+    );
+  }, [health]);
+
   return (
     <div className="health-bar-container">
-      <div className="health-bar">Health: {health}</div>
+      <div className="health-number">{health}</div>
+      <div className="health-bar"></div>
     </div>
   );
 }
